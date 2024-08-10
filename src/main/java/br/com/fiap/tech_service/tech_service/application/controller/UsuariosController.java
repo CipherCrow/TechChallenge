a96ceb1a5f11a0b7534e82b4cec523908fa6606d
CipherCrow <jonastorquatro@gmail.com>
2024-08-10 12:00:03 -0300

    Adicionando padrao do CRUD para usuarios

--- src/main/java/br/com/fiap/tech_service/tech_service/application/controller/UsuariosController.java
+++ src/main/java/br/com/fiap/tech_service/tech_service/application/controller/UsuariosController.java
@@ -5,10 +5,8 @@ import br.com.fiap.tech_service.tech_service.domain.entities.Usuarios;
 import br.com.fiap.tech_service.tech_service.application.mapper.UsuariosMapper;
 import br.com.fiap.tech_service.tech_service.domain.service.UsuariosService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/usuarios")
@@ -21,6 +19,24 @@ public class UsuariosController {
     public UsuariosDTO criarUsuario(@RequestParam String nome, @RequestParam String email) {
         Usuarios usuario = usuarioService.criarUsuario(nome, email);
         return UsuariosMapper.toDTO(usuario);
+    }
+
+    @GetMapping("/buscar")
+    public UsuariosDTO buscarUsuario(@RequestParam Long idUsuario) {
+        Usuarios usuario = usuarioService.buscarUsuario(idUsuario);
+        return UsuariosMapper.toDTO(usuario);
+    }
+
+    @PutMapping("/atualizar")
+    public UsuariosDTO atualizarUsuario(@RequestParam Long idUsuario,@RequestBody UsuariosDTO usuariosDTO) {
+        Usuarios usuarioAtualizado = usuarioService.atualizarUsuario(idUsuario, usuariosDTO);
+        return UsuariosMapper.toDTO(usuarioAtualizado);
+    }
+
+    @DeleteMapping("/deletar")
+    public ResponseEntity<Object> deletarUsuario(@RequestParam Long idUsuario) {
+        ResponseEntity<Object> usuarioDeletado = usuarioService.deletarUsuario(idUsuario);
+        return usuarioDeletado;
     }
 
 }
--- src/main/java/br/com/fiap/tech_service/tech_service/domain/service/UsuariosService.java
+++ src/main/java/br/com/fiap/tech_service/tech_service/domain/service/UsuariosService.java
@@ -1,9 +1,15 @@
 package br.com.fiap.tech_service.tech_service.domain.service;
 
+import br.com.fiap.tech_service.tech_service.application.dto.UsuariosDTO;
+import br.com.fiap.tech_service.tech_service.application.mapper.UsuariosMapper;
 import br.com.fiap.tech_service.tech_service.domain.entities.Usuarios;
 import br.com.fiap.tech_service.tech_service.domain.repository.UsuariosRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.NoSuchElementException;
 
 @Service
 public class UsuariosService {
@@ -16,5 +22,23 @@ public class UsuariosService {
         usuario.setNome(nome);
         usuario.setEmail(email);
         return usuarioRepository.save(usuario);
+    }
+
+    public Usuarios buscarUsuario(long idUsuario) {
+        Usuarios usuario = usuarioRepository.findById(idUsuario).orElseThrow(() -> new RuntimeException("Usuário não encontrado"));
+        return usuario;
+    }
+
+    public Usuarios atualizarUsuario(long idUsuario, UsuariosDTO usuariosDTO) {
+        Usuarios usuario = buscarUsuario(idUsuario);
+        usuario.setNome(usuariosDTO.nome());
+        usuario.setEmail(usuariosDTO.email());
+        usuario = usuarioRepository.save(usuario);
+        return usuario;
+    }
+    public ResponseEntity<Object> deletarUsuario(long idUsuario) {
+        Usuarios usuario =  buscarUsuario(idUsuario);
+        usuarioRepository.delete(usuario);
+        return ResponseEntity.accepted().build();
     }
 }
